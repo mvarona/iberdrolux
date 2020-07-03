b39c898dc6fdcb749e7902daef6dda87367153c4
mvb <mvaronabueno@gmail.com>
2020-07-04 01:27:51 +0200

    File reading added

--- iberdrolux.py
+++ iberdrolux.py
@@ -1,5 +1,20 @@
 import array
 import datetime
+import json
+from calendar import monthrange
+
+# Constants:
+
+MIN_HOUR = 0
+MAX_HOUR = 24
+MIN_DAY = 1
+MAX_DAY = 31
+MIN_MONTH = 1
+MAX_MONTH = 12
+MIN_YEAR = 1900
+MAX_YEAR = 2100
+
+# Classes:
 
 class Day:
 
@@ -27,6 +42,7 @@ class Year:
     self.months = None
     self.offset = None
 
+# Functions:
 
 def ensureInputRange(msg, min, max):
   userInput = int(input(msg))
@@ -55,6 +71,14 @@ def ensureFloat(msg):
   userInput = float(input(msg))
   return userInput
 
+def ensureString(msg):
+  userInput = str(input(msg))
+  return userInput
+
+def getNumberDaysForMonth(month, year):
+  num = monthrange(year, month)
+  return num[1]
+
 def showMenu():
   print("*** Bienvenido a Iberdrolux ***")
   print("** Calculador de precios para tarifas eléctricas")
@@ -65,19 +89,33 @@ def showMenu():
   print("Recuerde que el programa calcula una tarifa ad-hoc por ejecución, por lo que en caso de aplicarse más de una tarifa distinta en la factura anual (por ejemplo, horarios distintos en invierno o en verano), se debe fragmentar el conjunto de datos y aplicar individualmente el cálculo a cada fragmento. Es decir, en el caso de una factura anual con distinción estacionaria, se debería usar un conjunto de datos para invierno y otro para verano.")
   print("")
 
-  firstDay = ensureInputRange("Introduzca el primer día del conjunto de datos (1-31): ", 1, 31)
-  firstMonth = ensureInputRange("Introduzca el primer mes del conjunto de datos (1-12): ", 1, 12)
-  year = ensureInputRange("Introduzca el año del conjunto de datos (yyyy): ", 1900, 2100)
+  firstDay = ensureInputRange("Introduzca el primer día del conjunto de datos (1-31): ", MIN_DAY, MAX_DAY)
+  firstMonth = ensureInputRange("Introduzca el primer mes del conjunto de datos (1-12): ", MIN_MONTH, MAX_MONTH)
+  year = ensureInputRange("Introduzca el año del conjunto de datos (yyyy): ", MIN_YEAR, MAX_YEAR)
   power = ensureFloat("Introduzca la potencia contratada con punto (.) como delimitador de decimales (Ej.: 3.5) (kW/h): ")
   powerPrice = ensureFloat("Introduzca el precio por kW de potencia contratada, con punto (.) como delimitador de decimales: ")
   normalPrice = ensureFloat("Introduzca el precio del kW/h no bonificado, con punto (.) como delimitador de decimales: ")
   discountedPrice = ensureFloat("Introduzca el precio del kW/h bonificado, con punto (.) como delimitador de decimales: ")
-  beginDiscountHour = ensureInputRangeEmpty("Introduzca la hora de comienzo del precio bonificado (0-24, deje en blanco si no hay precio bonificado): ", 0, 24)
-  endDiscountHour = ensureInputRangeEmpty("Introduzca la hora de fin del precio bonificado (0-24, deje en blanco si no hay precio bonificado): ", 0, 24)
+  beginDiscountHour = ensureInputRangeEmpty("Introduzca la hora de comienzo del precio bonificado (0-24, deje en blanco si no hay precio bonificado): ", MIN_HOUR, MAX_HOUR)
+  endDiscountHour = ensureInputRangeEmpty("Introduzca la hora de fin del precio bonificado (0-24, deje en blanco si no hay precio bonificado): ", MIN_HOUR, MAX_HOUR)
+  filename = ensureString("Introduzca el nombre del archivo .json donde se encuentra el conjunto de datos: ")
 
-  return firstDay, firstMonth, year, power, powerPrice, normalPrice, discountedPrice, beginDiscountHour, endDiscountHour
+
+  return firstDay, firstMonth, year, power, powerPrice, normalPrice, discountedPrice, beginDiscountHour, endDiscountHour, filename
+
+def getFile(filename):
+  with open(filename) as file:
+    data = json.load(file)
+
+  data = data['y']['data'][0]
+
+  return data
 
 
 # Entry point:
 
-firstDay, firstMonth, year, power, powerPrice, normalPrice, discountedPrice, beginDiscountHour, endDiscountHour = showMenu()
+#firstDay, firstMonth, year, power, powerPrice, normalPrice, discountedPrice, beginDiscountHour, endDiscountHour, filename = showMenu()
+
+filename = "test_2017.json"
+data = getFile(filename)
+
