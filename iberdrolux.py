40e8f18de8b2c9e840890d9c44021b6c0290a1fc
mvb <mvaronabueno@gmail.com>
2020-07-04 00:44:57 +0200

    Input checking added

--- iberdrolux.py
+++ iberdrolux.py
@@ -28,6 +28,33 @@ class Year:
     self.offset = None
 
 
+def ensureInputRange(msg, min, max):
+  userInput = int(input(msg))
+  while userInput > max or userInput < min:
+    userInput = int(input(msg))
+
+  return userInput
+
+def ensureInputRangeEmpty(msg, min, max):
+  userInput = input(msg)
+  if (len(str(userInput)) == 0):
+    return
+  else:
+    userInput = int(userInput)
+
+  while userInput > max or userInput < min:
+    userInput = input(msg)
+    if (len(str(userInput)) == 0):
+      return
+    else:
+      userInput = int(userInput)
+
+  return userInput
+
+def ensureFloat(msg):
+  userInput = float(input(msg))
+  return userInput
+
 def showMenu():
   print("*** Bienvenido a Iberdrolux ***")
   print("** Calculador de precios para tarifas eléctricas")
@@ -38,15 +65,15 @@ def showMenu():
   print("Recuerde que el programa calcula una tarifa ad-hoc por ejecución, por lo que en caso de aplicarse más de una tarifa distinta en la factura anual (por ejemplo, horarios distintos en invierno o en verano), se debe fragmentar el conjunto de datos y aplicar individualmente el cálculo a cada fragmento. Es decir, en el caso de una factura anual con distinción estacionaria, se debería usar un conjunto de datos para invierno y otro para verano.")
   print("")
 
-  firstDay = input("Introduzca el primer día del conjunto de datos (0-31): ")
-  firstMonth = input("Introduzca el primer mes del conjunto de datos (1-12): ")
-  year = input("Introduzca el año del conjunto de datos (yyyy): ")
-  power = input("Introduzca la potencia contratada (kW/h): ")
-  powerPrice = input("Introduzca el precio por kW (potencia): ")
-  normalPrice = input("Introduzca el precio del kW/h no bonificado: ")
-  discountedPrice = input("Introduzca el precio del kW/h bonificado: ")
-  beginDiscountHour = input("Introduzca la hora de comienzo del precio bonificado (0-24, deje en blanco si no hay precio bonificado): ")
-  endDiscountHour = input("Introduzca la hora de fin del precio bonificado (0-24, deje en blanco si no hay precio bonificado): ")
+  firstDay = ensureInputRange("Introduzca el primer día del conjunto de datos (1-31): ", 1, 31)
+  firstMonth = ensureInputRange("Introduzca el primer mes del conjunto de datos (1-12): ", 1, 12)
+  year = ensureInputRange("Introduzca el año del conjunto de datos (yyyy): ", 1900, 2100)
+  power = ensureFloat("Introduzca la potencia contratada con punto (.) como delimitador de decimales (Ej.: 3.5) (kW/h): ")
+  powerPrice = ensureFloat("Introduzca el precio por kW de potencia contratada, con punto (.) como delimitador de decimales: ")
+  normalPrice = ensureFloat("Introduzca el precio del kW/h no bonificado, con punto (.) como delimitador de decimales: ")
+  discountedPrice = ensureFloat("Introduzca el precio del kW/h bonificado, con punto (.) como delimitador de decimales: ")
+  beginDiscountHour = ensureInputRangeEmpty("Introduzca la hora de comienzo del precio bonificado (0-24, deje en blanco si no hay precio bonificado): ", 0, 24)
+  endDiscountHour = ensureInputRangeEmpty("Introduzca la hora de fin del precio bonificado (0-24, deje en blanco si no hay precio bonificado): ", 0, 24)
 
   return firstDay, firstMonth, year, power, powerPrice, normalPrice, discountedPrice, beginDiscountHour, endDiscountHour
 
