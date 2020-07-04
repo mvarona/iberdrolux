5f219140939ac28bc6196d8f78a530eff3de050c
mvb <mvaronabueno@gmail.com>
2020-07-04 13:21:42 +0200

    Added main loop to populate date, not tested

--- iberdrolux.py
+++ iberdrolux.py
@@ -20,27 +20,26 @@ class Day:
 
   def __init__(self, num):
     self.num = num
-    self.cost = None
-    self.consumption = None
+    self.cost = 0.00
+    self.consumption = 0.00
     self.hourly_consumption = None
 
 class Month:
 
   def __init__(self, num):
     self.num = num
-    self.cost = None
-    self.consumption = None
+    self.cost = 0.00
+    self.consumption = 0.00
     self.days = None
 
 class Year:
 
-  def __init__(self, num, offset):
+  def __init__(self, num):
     self.num = num
     self.offset = offset
-    self.cost = None
-    self.consumption = None
+    self.cost = 0.00
+    self.consumption = 0.00
     self.months = None
-    self.offset = None
 
 # Functions:
 
@@ -50,6 +49,7 @@ def ensureInputRange(msg, min, max):
     userInput = int(input(msg))
 
   return userInput
+
 
 def ensureInputRangeEmpty(msg, min, max):
   userInput = input(msg)
@@ -67,17 +67,21 @@ def ensureInputRangeEmpty(msg, min, max):
 
   return userInput
 
+
 def ensureFloat(msg):
   userInput = float(input(msg))
   return userInput
+
 
 def ensureString(msg):
   userInput = str(input(msg))
   return userInput
 
+
 def getNumberDaysForMonth(month, year):
   num = monthrange(year, month)
   return num[1]
+
 
 def showMenu():
   print("*** Bienvenido a Iberdrolux ***")
@@ -92,16 +96,16 @@ def showMenu():
   firstDay = ensureInputRange("Introduzca el primer día del conjunto de datos (1-31): ", MIN_DAY, MAX_DAY)
   firstMonth = ensureInputRange("Introduzca el primer mes del conjunto de datos (1-12): ", MIN_MONTH, MAX_MONTH)
   year = ensureInputRange("Introduzca el año del conjunto de datos (yyyy): ", MIN_YEAR, MAX_YEAR)
-  power = ensureFloat("Introduzca la potencia contratada con punto (.) como delimitador de decimales (Ej.: 3.5) (kW/h): ")
-  powerPrice = ensureFloat("Introduzca el precio por kW de potencia contratada, con punto (.) como delimitador de decimales: ")
-  normalPrice = ensureFloat("Introduzca el precio del kW/h no bonificado, con punto (.) como delimitador de decimales: ")
-  discountedPrice = ensureFloat("Introduzca el precio del kW/h bonificado, con punto (.) como delimitador de decimales: ")
+  power = ensureFloat("Introduzca la potencia contratada con punto (.) como delimitador de decimales (Ej.: 3.5) (kW/año): ")
+  powerPrice = ensureFloat("Introduzca el precio en euros por kW/año de potencia contratada, con punto (.) como delimitador de decimales: ")
+  normalPrice = ensureFloat("Introduzca el precio en euros del kW/h no bonificado, con punto (.) como delimitador de decimales: ")
+  discountedPrice = ensureFloat("Introduzca el precio en euros del kW/h bonificado, con punto (.) como delimitador de decimales: ")
   beginDiscountHour = ensureInputRangeEmpty("Introduzca la hora de comienzo del precio bonificado (0-24, deje en blanco si no hay precio bonificado): ", MIN_HOUR, MAX_HOUR)
   endDiscountHour = ensureInputRangeEmpty("Introduzca la hora de fin del precio bonificado (0-24, deje en blanco si no hay precio bonificado): ", MIN_HOUR, MAX_HOUR)
   filename = ensureString("Introduzca el nombre del archivo .json donde se encuentra el conjunto de datos: ")
 
-
   return firstDay, firstMonth, year, power, powerPrice, normalPrice, discountedPrice, beginDiscountHour, endDiscountHour, filename
+
 
 def getFile(filename):
   with open(filename) as file:
@@ -112,10 +116,59 @@ def getFile(filename):
   return data
 
 
+def calculateCostForDay(hourly_consumption, power, powerPrice, normalPrice, discountedPrice, beginDiscountHour, endDiscountHour):
+
+  
+
+  return cost
+
+
+def getNextMonth(month):
+  if (month != MAX_MONTH):
+    nextMonth = month + 1
+  else:
+    nextMonth = MIN_MONTH
+
+  return nextMonth
+
+
 # Entry point:
 
 #firstDay, firstMonth, year, power, powerPrice, normalPrice, discountedPrice, beginDiscountHour, endDiscountHour, filename = showMenu()
 
 filename = "test_2017.json"
-data = getFile(filename)
+hours = getFile(filename)
 
+if (firstDay > getNumberDaysForMonth(firstMonth, year)):
+  print("El día inicial del conjunto de datos no puede ser mayor que el número de días que tiene el mes al que pertenece")
+  return
+
+numDay = firstDay
+numMonth = firstMonth
+numYear = year
+
+day = Day(numDay)
+month = Month(numMonth)
+year = Year(numYear)
+
+for hour in hours:
+
+  day.hourly_consumption.append(hour['valor'])
+
+  if (len(day.hourly_consumption) == MAX_HOUR or hour == hours[-1]):
+    day.consumption = sum(day.hourly_consumption)
+    day.cost = calculateCostForDay(day.hourly_consumption, power, powerPrice, normalPrice, discountedPrice, beginDiscountHour, endDiscountHour)
+
+    month.days.append(day)
+    numDay += 1
+
+    if (numDay == getNumberDaysForMonth(numMonth, numYear)):
+      year.months.append(month)
+      numDay = MIN_DAY
+      numMonth = getNextMonth(numMonth)
+      if (numMonth == MIN_MONTH):
+        numYear += 1
+        year = Year(numYear)
+      month = Month(numMonth)
+
+    day = Day(numDay)
