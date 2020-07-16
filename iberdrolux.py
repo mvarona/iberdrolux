ac9f84c576e1857f081f5f7d14266b718791bc77
mvb <mvaronabueno@gmail.com>
2020-07-17 00:51:51 +0200

    Refactoring. Thanks to @MikenTNT

--- iberdrolux.py
+++ iberdrolux.py
@@ -189,30 +189,8 @@ def getNextMonth(month):
 
 
 def getNameForMonth(month):
-  if (month == 1):
-    return "Enero"
-  if (month == 2):
-    return "Febrero"
-  if (month == 3):
-    return "Marzo"
-  if (month == 4):
-    return "Abril"
-  if (month == 5):
-    return "Mayo"
-  if (month == 6):
-    return "Junio"
-  if (month == 7):
-    return "Julio"
-  if (month == 8):
-    return "Agosto"
-  if (month == 9):
-    return "Septiembre"
-  if (month == 10):
-    return "Octubre"
-  if (month == 11):
-    return "Noviembre"
-  if (month == 12):
-    return "Diciembre"
+  monthsNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+  return monthsNames[month-1]
 
 
 def showSummary(power, powerPrice, normalPrice, discountedPrice, beginDiscountHour, endDiscountHour, filename):
